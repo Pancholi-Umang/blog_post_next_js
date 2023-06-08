82a22a2f3089d68cf01c16d1d401aad99ed33c0e
unknown <umangpancholi555@gmail.com>
2023-06-08 12:55:21 +0530

    complate sir task

--- pages/components/cart/[token].js
+++ pages/components/cart/[token].js
@@ -93,46 +93,64 @@ const cart = () => {
 
   useEffect(() => {
     if (tokendata?.length !== 0) {
-      removeUser(logindata, tokendata)
+      removeUser(logindata)
     }
   }, [change?.length !== 0])
 
-  const removeUser = async (logindata, tokendata) => {
-    console.log(change)
-    console.log(logindata, tokendata) 
+  const removeUser = async (logindata) => {
     if (token != undefined && User != undefined) {
       if (token != User) {
-       if (logindata?.length != 0) {
-        await Promise.all(
-          loggerdata.map(async (datas) => {
-            await axios.delete(`http://192.168.29.229:5000/cart/${datas?.id}`);
-          })
-        );
+        if (logindata?.length != 0) {
+          await Promise.all(
+            loggerdata.map(async (datas) => {
+              await axios.delete(`http://192.168.29.229:5000/cart/${datas?.id}`);
+            })
+          );
         }
-          change?.map((val) => {
-            axios?.post(`http://192.168.29.229:5000/cart`, val)
-              .then((res) => {
-                console.log(res?.data);
-              })
-          }); 
-        }
+        change?.map((val) => {
+          axios?.post(`http://192.168.29.229:5000/cart`, val)
+            .then((res) => {
+              console.log(res?.data);
+            })
+        });
+      }
     }
   }
+  const STATUSES = Object.freeze({
+    IDLE: "idle",
+    ERROR: "error",
+    LOADING: "loading",
+  });
+
+  const [idles, setidles] = useState(STATUSES?.LOADING)
+  const [CouponMinusValue, GetCouponMinusValue] = useState(0)
 
   const handleApplyCoupon = () => {
     if (coupon === "save10") {
       let minus = (cartTotal * 10) / 100;
+      GetCouponMinusValue((cartTotal * 10) / 100)
       setCouponPrice(cartTotal - minus);
+      setidles(STATUSES?.IDLE)
     }
     else if (coupon === "save30") {
       let minus = (cartTotal * 30) / 100;
+      GetCouponMinusValue((cartTotal * 30) / 100)
       setCouponPrice(cartTotal - minus);
+      setidles(STATUSES?.IDLE)
     }
     else {
       setCouponPrice(cartTotal);
+      setidles(STATUSES?.ERROR)
+      GetCouponMinusValue(0)
     }
   }
 
+  const [NumberActive, setNumberActive] = useState(0)
+
+  const handleChhoseDelivery = (price) => {
+    setNumberActive(price)
+  }
+  
   return (
     <>
       <Head>
@@ -172,10 +190,7 @@ const cart = () => {
                         </div>
                         <div className="col-md-2 d-flex justify-content-center">
                           <div>
-                            <p className="small text-muted mb-4 pb-2">
-
-                              Quantity
-                            </p>
+                            <p className="small text-muted mb-4 pb-2"> Quantity </p>
                             <p className="lead fw-normal mb-0 text-center d-flex justify-content-between">
                               {User == token ? (
                                 <span
@@ -189,10 +204,7 @@ const cart = () => {
                                 > -
                                 </span>
                               ) : (
-                                <span className="myPointer text-center user-select-none">
-
-                                  -
-                                </span>
+                                <span className="myPointer text-center user-select-none"> - </span>
                               )}
 
                               <input
@@ -255,21 +267,69 @@ const cart = () => {
               }, [])}
 
               <div className="card mb-5">
-                <div className="card-body p-4">
-                  <div className="float-end">
-                    <p className="mb-0 me-5 d-flex align-items-center">
+                <div className="card-body p-4 d-flex align-items-center justify-content-between row">
+                  <div className="d-flex align-items-center justify-content-between col-md-5">
+
+                    <button className={`btn btn-sq-lg btn-outline-dark ${NumberActive == 0 ? "active" : null}`} onClick={() => handleChhoseDelivery(Number(0))}>
+                      Free <br /> Delivery
+                    </button>
+
+                    <button className={`btn btn-sq-lg btn-outline-dark ${NumberActive == 5 ? "active" : null}`} onClick={() => handleChhoseDelivery(Number(5))}>
+                      Fast <br /> Delivery <br /> ₹5
+                    </button>
+
+                    <button className={`btn btn-sq-lg btn-outline-dark ${NumberActive == 10 ? "active" : null}`} onClick={() => handleChhoseDelivery(Number(10))}>
+                      Express <br /> Delivery <br /> ₹10
+                    </button>
+                  </div>
+                  <div className="col-md-4">
+                    <p className="mb-0 me-5 d-flex align-items-center justify-content-evenly  px-3">
+                      <span className="small text-muted me-2">
+                        Sub total:
+                      </span>
+                      <span className="lead fw-normal">₹{cartTotal}</span>
+                    </p>
+                    <p className="mb-0 me-5 d-flex align-items-center justify-content-evenly  px-3">
+                      <span className="small text-muted me-2">
+                        Coupon:
+                      </span>
+                      <span className="lead fw-normal">₹{CouponMinusValue}</span>
+                    </p>
+                    <p className="mb-0 me-5 d-flex align-items-center justify-content-evenly  px-3">
+                      <span className="small text-muted me-2">
+                        delivery:
+                      </span>
+                      <span className="lead fw-normal">₹{NumberActive}</span>
+                    </p>
+                    <p className="mb-0 me-5 d-flex align-items-center justify-content-evenly  px-3">
                       <span className="small text-muted me-2">
                         Order total:
                       </span>
-                      <span className="lead fw-normal">₹{coupanPrice == 0 ? cartTotal : coupanPrice}</span>
+                      <span className="lead fw-normal">₹{coupanPrice == 0 ? cartTotal + NumberActive : coupanPrice + NumberActive}</span>
+                      {/* aama shu karvanu chhe khabr condition to tej reva devani chhe but khali chhe ne couponprice * delivery kari devanu */}
+                      {/* <span className="lead fw-normal">₹{coupanPrice == 0 ? cartTotal + 5 : coupanPrice + 5}</span> */}
                     </p>
+
                   </div>
                 </div>
               </div>
 
+              <div className="d-flex justify-content-end mb-5">
+                <span className="position-relative">
+                  <input type="text" className="border-0 bg-light inputTaggged rounded ps-2" value={coupon} onChange={(e) => setCoupon(e.target.value)} placeholder="Enter Coupon..." />
+
+                  {(() => {
+                    switch (idles) {
+                      case "idle": return <p className="position-absolute text-success">Coupon Applied!</p>;
+                      case "error": return <p className="position-absolute text-danger">Please Enter Valid Coupon</p>;
+                      default: return "";
+                    }
+                  })()}
+
+                </span>
+                <button onClick={() => handleApplyCoupon()} className="btn btn-light ms-2">Apply Coupon</button>
+              </div>
               <div className="d-flex justify-content-end">
-                <input type="text" className="ps-2 border-0 me-2 bg-light inputTaggged rounded" value={coupon} onChange={(e) => setCoupon(e.target.value)} placeholder="Enter Coupon..." />
-                <button onClick={() => handleApplyCoupon()} className="btn btn-light mx-2 ">Apply Coupon</button>
                 <Link href="/" type="button" className="btn btn-light me-2">
                   Continue shopping
                 </Link>
